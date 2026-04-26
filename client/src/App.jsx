8686b7a8e33c0a6e9f523339be5deb825f6a12c2
Cam <camerons.cmu@gmail.com>
2026-04-26 16:34:54 -0500

    Wire FilterProvider and TopNav into App shell

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,20 @@
 // client/src/App.jsx
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FilterProvider } from './context/FilterContext';
+import { TopNav } from './components/TopNav/TopNav';
 import Home from './pages/Home';
 import Rep from './pages/Rep';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/rep/:id" element={<Rep />} />
-      </Routes>
+      <FilterProvider>
+        <TopNav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rep/:id" element={<Rep />} />
+        </Routes>
+      </FilterProvider>
     </BrowserRouter>
   );
 }
